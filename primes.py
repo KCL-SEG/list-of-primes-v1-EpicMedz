9e82cfe84eeeba981e016170bba24548cd41672f
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2022-09-28 22:46:57 +0100

    First attempt at implementing primes function

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    list.append(2)
+    for number in range(2, number_of_primes):
+        for num in range(2,number):
+            if number % num == 0:
+                break
+            elif number not in list:
+                list.append(number)
+    print(f"{list}")
     return list
+
+
+
+
+primes(10)
