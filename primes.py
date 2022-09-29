47308dd3be695f5723bf8c08145bbf799cb57de6
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2022-09-29 09:55:49 +0100

    Attempt primes function for the 3rd time

--- primes.py
+++ primes.py
@@ -3,11 +3,23 @@
 
 def primes(number_of_primes):
     list = []
-    for number in range(3, number_of_primes + 1):
-        for num in range(2, number):
-            if number % num == 0:
-                break
-        else:
-            list.append(number)
+    for num in range(1, number_of_primes + 1):
+        if num > 1:
+            for i in range(2,num):
+                if (num % i) == 0:
+                    break
+            else:
+                list.append(num)
+        elif number_of_primes == 1:
+            list.append(2)
+            break
+        elif number_of_primes == 2:
+            list.append(2)
+            list.append(3)
+            break
     print(f"{list}")
     return list
+
+primes(20)
+primes(1)
+primes(2)
